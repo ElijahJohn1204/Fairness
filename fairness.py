1f96b7b6bb48cc0a7e0c67a5b074f1549f2fd965
ElijahJohn1204 <elijahjohn@ameritech.net>
2025-07-25 08:36:20 -0500

    Added Graphs to notebook and python file

--- fairness.py
+++ fairness.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score, f1_score
+from sklearn.metrics import accuracy_score, f1_score, roc_curve
 from sklearn.preprocessing import StandardScaler
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.linear_model import LogisticRegression
